92bbfc2d9f3a41939d2677f8bd361166b63e74c5
Zach <zacktesolin13@gmail.com>
2023-06-19 22:34:56 -0400

    Create contact form and connect to contact service

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { ContactService } from '../services/contact.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+	selector: 'app-contact',
+	templateUrl: './contact.component.html',
+	styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+	userContactFormGroup: FormGroup = new FormGroup({
+		name: new FormControl(''),
+		email: new FormControl(''),
+		subject: new FormControl(''),
+		message: new FormControl(''),
+	});
 
+	constructor(private contactService: ContactService) {}
+
+	onSubmit() {
+		this.contactService
+			.sendContactEmail(this.userContactFormGroup.getRawValue())
+			.subscribe((response) => {
+				location.href = 'https://mailthis.to/confirm';
+			});
+	}
 }
